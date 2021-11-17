e90df8d5738eb15ed4ffc1c00d417438f3a9852e
Olga Lebedeva <birdswann@gmail.com>
2021-11-17 18:12:16 +0300

    Task 1 done

---
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -0,0 +1,65 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+    ProductManager manager = new ProductManager();
+    Product book1 = new Book(1, "Dune", 300, "Gerbert");
+    Product book2 = new Book(2, "Naruto", 500, "Kishimoto");
+    Product smartphone1 = new Smartphone(11, "Galaxy", 30_000, "Samsung");
+    Product smartphone2 = new Smartphone(22, "IPhone", 50_000, "Apple");
+    Product cat = new Product(1, "Sirius", 0);
+
+    @BeforeEach
+            public void setUp() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+        manager.add(cat);
+    }
+
+    @Test
+    void shouldSearchABookByName() {
+        Product[] actual = manager.searchBy("Naruto");
+        Product[] expected = new Product[]{book2};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldSearchByAuthor() {
+        Product[] actual = manager.searchBy("Gerbert");
+        Product[] expected = new Product[]{book1};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldSearchASmartphoneByName() {
+        Product[] actual = manager.searchBy("Galaxy");
+        Product[] expected = new Product[]{smartphone1};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldSearchByProducer() {
+        Product[] actual = manager.searchBy("Apple");
+        Product[] expected = new Product[]{smartphone2};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void shouldNotSearchNonExistent() {
+        Product[] actual = manager.searchBy("Sasuke");
+        Product[] expected = new Product[]{};
+        assertArrayEquals(actual, expected);
+    }
+    @Test
+    void shouldNotSearchACat() {
+        Product[] actual = manager.searchBy("Sirius");
+        Product[] expected = new Product[]{};
+        assertArrayEquals(actual, expected);
+    }
+}
